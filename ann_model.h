81848cca4acc15507ca099a6cd7c8bb0c6e50a46
Aadesh Manandhar <aadeshmanandhar14646@gmail.com>
2024-06-18 14:19:22 +0545

    ANN_MODEL complete!

---
+++ ann_model.h
@@ -0,0 +1,218 @@
+#ifndef ANN_MODEL
+#define ANN_MODEL
+
+#include<iostream>
+#include<vector>
+#include<random>
+#include<stdexcept>
+#include<thread>
+#include "dual.h"
+
+using std::vector;
+
+//Some notes:
+
+//Momentum has to be set separately and cannot be set through the .fit function, initial value = 0.1
+//The value of the target during categorical_crossentropy must begin with 0, -> because it is used to index the y_train array when calculating the loss
+
+//multi_threading is done for calculation of partial derivatives wrt various parameters. (each thread calculates wrt to some parameters and thus the work is divided !)
+
+//multi_threaded matrix multiplication function (matmul) is also made available but not used, instead single_threaded one is used (originalMatMul)
+
+enum class activations { linear, sigmoid, softmax, relu, tanh};
+enum class lossFunction { mean_squared_error, binary_crossentropy, categorical_crossentropy };
+
+class ANN_Model {
+    private:
+        vector<DualNum> parameters;
+        int inputs;
+        vector<activations> activationFunctions;
+        vector<int> nodes;
+        vector<double> velocity;
+        double momentum = 0.1;
+
+    public:
+        ANN_Model(int inputFeatures, vector<int> No_of_Nodes, vector<activations> activationFunctionName) {
+            inputs = inputFeatures;
+
+            nodes = No_of_Nodes;
+
+            activationFunctions = activationFunctionName;
+
+            std::random_device rd;
+            std::mt19937 gen(rd());
+
+            std::uniform_real_distribution<> distrib(-0.5, 0.5);
+
+            long int no_of_parameters = inputFeatures * No_of_Nodes[0] + No_of_Nodes[0];
+
+
+            for (int i = 0; i < No_of_Nodes.size() - 1; i++){
+                no_of_parameters += No_of_Nodes[i] * No_of_Nodes[i+1] + No_of_Nodes[i+1];
+            }
+
+
+            for (int i = 0; i < no_of_parameters; i++){
+                double random_number = distrib(gen);
+
+                parameters.push_back(DualNum("p:" + std::to_string(i), random_number));
+            }
+
+            velocity = vector<double>(parameters.size(), 0);
+        }
+
+        vector<DualNum>& getParameters(){
+            return parameters;
+        }
+
+        void setMomentum(double m){
+            momentum = m;
+        }
+
+        vector<DualNum> predict(vector<DualNum> X, vector<DualNum> params = {}){
+            if (params.size() == 0){
+                params = parameters;
+            }
+
+            if (X.size() != inputs){
+                throw std::runtime_error("The shape of the inputs isn't right!");
+            }
+
+            vector<DualNum> inputVect = X;
+
+            int start = 0;
+
+            for (int i = 0; i < nodes.size(); i++){
+                vector<vector<DualNum>> currentWeightMatrix;
+
+
+                for (int j = 0; j < inputVect.size();  j++){
+                    int end = start + nodes[i];
+
+                    vector<DualNum> weightForEachNode(params.begin() + start, params.begin() + end);
+
+                    currentWeightMatrix.push_back(weightForEachNode);
+
+                    start = end;
+                }
+
+                vector<DualNum> currentBiasVect(params.begin() + start, params.begin() + start + nodes[i]);
+
+                start += nodes[i];
+
+                vector<DualNum> tempVect = Dual::originalMatMul(vector<vector<DualNum>>(1, inputVect), currentWeightMatrix)[0];
+
+                DualNum expSums("sums", 0, 0);
+                if (activationFunctions[i] == activations::softmax){
+                    for (int j = 0; j < tempVect.size(); j++){
+                        expSums += Dual::exp(tempVect[j] + currentBiasVect[j]);
+                    }
+                }
+
+                for (int j = 0; j < tempVect.size(); j++){
+                    tempVect[j] += currentBiasVect[j];
+
+                    if (activationFunctions[i] == activations::relu){
+                        tempVect[j] = Dual::relu(tempVect[j]);
+                    } else if (activationFunctions[i] == activations::sigmoid){
+                        tempVect[j] = Dual::sigmoid(tempVect[j]);
+                    } else if (activationFunctions[i] == activations::tanh){
+                        tempVect[j] = Dual::tanh(tempVect[j]);
+                    } else if (activationFunctions[i] == activations::softmax){
+                        tempVect[j] = Dual::softmax(tempVect, j, expSums);
+                    }
+                }
+
+             inputVect = tempVect;
+            }
+
+            return inputVect;
+        }
+
+        vector<vector<DualNum>> predictAll(vector<vector<DualNum>> data, vector<DualNum> params = {}){
+            if (data[0].size() != inputs){
+                throw std::runtime_error("The shape of the inputs isn't right!");
+            }
+
+            vector<vector<DualNum>> entireOutputs;
+
+            for (int i = 0; i < data.size(); i++){
+                entireOutputs.push_back(predict(data[i], params));
+            }
+
+            return entireOutputs;
+        }
+
+        template <typename Func>
+        void updateParameters(double learning_rate, Func loss, vector<DualNum>& newParameters, int start, int end){
+            for (int i = start; i < end; i++){
+                double gradient = partialDerivative(loss, parameters, i, parameters[i].getReal());
+                velocity[i] = momentum * velocity[i] - learning_rate * gradient;
+                newParameters[i] = parameters[i] + velocity[i];
+            }
+        }
+
+        void fit(vector<vector<DualNum>> X_train, vector<DualNum> y_train, lossFunction lossFunc = lossFunction::mean_squared_error, double learning_rate = 0.01, int epochs = 100, int verbose = 2){
+            if (y_train.size() != X_train.size()){
+                throw std::runtime_error("The shape of the X_train and y_train don't match !");
+            }
+
+            auto loss = [X_train, y_train, this, lossFunc](vector<DualNum> params) mutable {
+                vector<vector<DualNum>> yhat = this->predictAll(X_train, params);
+
+                DualNum result("result", 0, 0);
+
+                for (int i = 0; i < yhat.size(); i++){
+                    if (lossFunc == lossFunction::mean_squared_error){
+                        result += (y_train[i] - yhat[i][0]) * (y_train[i] - yhat[i][0]);
+                    } else if (lossFunc == lossFunction::binary_crossentropy){
+                        result -= (y_train[i] * Dual::log(yhat[i][0]) + (1 - y_train[i]) * Dual::log(1 - yhat[i][0]));
+                    } else if (lossFunc == lossFunction::categorical_crossentropy){
+                        result -= Dual::log(yhat[i][y_train[i].getReal()]);
+                    }
+                }
+
+                return result / (double)(yhat.size());
+            };
+
+            int no_of_threads = std::thread::hardware_concurrency();
+
+            for (int epoch = 1; epoch <= epochs; epoch++){
+                vector<DualNum> newParameters(parameters.size(), DualNum("dummyParam", 0, 0));
+
+                vector<std::thread> threads;
+
+                if (no_of_threads >= parameters.size()){
+                    for (int i = 0; i < parameters.size(); i++){
+                        threads.emplace_back(&ANN_Model::updateParameters<decltype(loss)>, this, learning_rate, loss, std::ref(newParameters), i, i+1);
+                    }
+                } else {
+                    int start = 0;
+                    int portion_for_each = parameters.size() / no_of_threads;
+
+                    for(int i = 0; i < no_of_threads; i++){
+                        int end = (i == (no_of_threads - 1) ? parameters.size() : start + portion_for_each);
+                        threads.emplace_back(&ANN_Model::updateParameters<decltype(loss)>, this, learning_rate, loss, std::ref(newParameters), start, end);
+                        start = end;
+                    }
+                }
+
+                for (auto& thread: threads){
+                    thread.join();
+                }
+
+                parameters = newParameters;
+
+                auto loss_function = Dual::mse;
+
+                if (verbose == 2){
+                    std::cout << "\n Epoch no: " << epoch << " completed!, Loss = "  << loss_function(y_train, this->predictAll(X_train)).getReal()  << std::endl;
+                } else if (verbose == 1){
+                    std::cout << "\n Epoch no: " << epoch << " completed!" << std::endl;
+                }
+
+            }
+        }
+};
+
+#endif
---
+++ dual.h
@@ -0,0 +1,448 @@
+#ifndef DUALNUMBER
+#define DUALNUMBER
+
+#include<iostream>
+#include<cmath>
+#include<string>
+#include<vector>
+#include<stdexcept>
+#include<thread>
+
+using std::vector;
+using std::string;
+
+class DualNum {
+    private:
+        string name;
+        long double real;
+        long double dual;
+
+    public:
+        DualNum(string n, long double r = 0, long double e = 0){
+            name = n;
+            real = r;
+            dual = e;
+        }
+
+        DualNum(const DualNum& num){
+            name = num.name;
+            real = num.real;
+            dual = num.dual;
+        }
+
+        long double getReal(){
+            return real;
+        }
+
+        long double getDual(){
+            return dual;
+        }
+
+        void setReal(long double r){
+            real = r;
+        }
+
+        void setDual(long double e){
+            dual = e;
+        }
+
+        string getName(){
+
+            return name;
+        }
+
+        string getExpression(){
+            return std::to_string(real) + " + " + std::to_string(dual) + "E";
+        }
+
+        void setName(string n){
+            name = n;
+        }
+
+        void operator = (long double realVal){
+            real = realVal;
+        }
+
+        DualNum operator * (DualNum num){
+            return DualNum("(" + name + "*" + num.name + ")", real * num.real, (real * num.dual + dual * num.real));
+        }
+
+        void operator *= (DualNum num){
+            name = "(" + name + "*" + num.name + ")";
+
+            long double tempReal = real * num.real;
+
+            long double tempDual = (real * num.dual + dual * num.real);
+
+            real = tempReal;
+            dual = tempDual;
+        }
+
+        DualNum operator * (const long double k){
+            return DualNum("(" + name + "*" + std::to_string(k) + ")", real * k, dual * k);
+        }
+
+        void operator *= (const long double k){
+            name = "(" + name + "*" + std::to_string(k) + ")";
+
+            long double tempReal =  real * k;
+
+            long double tempDual = dual * k;
+
+            real = tempReal;
+            dual = tempDual;
+        }
+
+        DualNum operator / (DualNum num){
+            return DualNum("(" + name + "/" + num.name + ")", real / num.real, (dual * num.real - real * num.dual) / (num.real * num.real));
+        }
+
+        void operator /= (DualNum num){
+            name = "(" + name + "/" + num.name + ")";
+
+            long double tempReal =  real / num.real;
+
+            long double tempDual = (dual * num.real - real * num.dual) / (num.real * num.real);
+
+            real = tempReal;
+            dual = tempDual;
+        }
+
+        DualNum operator / (const long double k){
+            return DualNum("(" + name + "/" + std::to_string(k) + ")", real / k, dual / k);
+        }
+
+        void operator /= (const long double k){
+            name = "(" + name + "/" + std::to_string(k) + ")";
+
+            long double tempReal =  real / k;
+
+            long double tempDual =  dual / k;
+
+            real = tempReal;
+            dual = tempDual;
+        }
+
+        DualNum operator + (DualNum num){
+            return DualNum("(" + name + "+" + num.name + ")", real + num.real, dual + num.dual);
+        }
+        
+        void operator += (DualNum num){
+            name = "(" + name + "+" + num.name + ")";
+
+            long double tempReal =  real + num.real;
+
+            long double tempDual =  dual + num.dual;
+
+            real = tempReal;
+            dual = tempDual;
+        }
+
+        DualNum operator + (const long double k){
+            return DualNum("(" + name + "+" + std::to_string(k) + ")", real + k, dual);
+        }
+
+        void operator += (const long double k){
+            name = "(" + name + "+" + std::to_string(k) + ")";
+
+            long double tempReal =  real + k;
+
+            long double tempDual =  dual;
+
+            real = tempReal;
+            dual = tempDual;
+        }
+
+        DualNum operator - (DualNum num){
+            return DualNum("(" +  name + "-" + num.name + ")", real - num.real, dual - num.dual);
+        }
+
+        void operator -= (DualNum num){
+            name = "(" +  name + "-" + num.name + ")";
+
+            long double tempReal =  real - num.real;
+
+            long double tempDual =  dual - num.dual;
+
+            real = tempReal;
+            dual = tempDual;
+        }
+
+        DualNum operator - (const long double k){
+            return DualNum("(" + name + "-" + std::to_string(k) + ")", real - k, dual);
+        }
+
+        void operator -= (const long double k){
+            name = "(" + name + "-" + std::to_string(k) + ")";
+
+            long double tempReal =  real - k;
+
+            long double tempDual =  dual;
+
+            real = tempReal;
+            dual = tempDual;
+        }
+
+        DualNum operator ^ (DualNum num){
+            long double realTerm = std::pow(real, num.real);
+
+            return DualNum("((" + name + ")" + "^" + "(" + num.name + "))", realTerm, realTerm * ((dual * num.real / real) + num.dual * std::log(real)));
+        }
+
+        void operator ^= (DualNum num){
+            long double realTerm = std::pow(real, num.real);
+
+            name = "((" + name + ")" + "^" + "(" + num.name + "))";
+
+            long double tempReal =  realTerm;
+
+            long double tempDual =  realTerm * ((dual * num.real / real) + num.dual * std::log(real));
+
+            real = tempReal;
+            dual = tempDual;
+        }
+
+        DualNum operator ^ (const long double k){
+            long double realTerm = std::pow(real, k);
+
+            return DualNum("((" + name + ")" + "^" + "(" + std::to_string(k) + "))", realTerm, realTerm * ((dual * k / real)));
+        }
+
+        void operator ^= (const long double k){
+            long double realTerm = std::pow(real, k);
+
+            name = "((" + name + ")" + "^" + "(" + std::to_string(k) + "))";
+
+            long double tempReal =  realTerm;
+
+            long double tempDual =  realTerm * ((dual * k / real));
+
+            real = tempReal;
+            dual = tempDual;
+        }
+};
+
+//Normal Numbers Operator Overloading
+
+DualNum operator * (long double k, DualNum num){
+    return DualNum("(" + std::to_string(k) + "*" + num.getName() + ")", k * num.getReal(), k * num.getDual());
+}
+
+DualNum operator / (long double k, DualNum num){
+    return DualNum("(" + std::to_string(k) + "/" + num.getName() + ")", k / num.getReal(), -1 * (k * num.getDual()) / (num.getReal() * num.getReal()));
+}
+
+DualNum operator + (long double k, DualNum num){
+    return DualNum("(" + std::to_string(k) + "+" + num.getName() + ")", k + num.getReal(), num.getDual());
+}
+
+DualNum operator - (long double k, DualNum num){
+    return DualNum("(" + std::to_string(k) + "-" + num.getName() + ")", k - num.getReal(), -1 * num.getDual());
+}
+
+DualNum operator ^ (long double k, DualNum num){
+    long double realTerm = std::pow(k, num.getReal());
+
+    return DualNum("((" + std::to_string(k) + ")" + "^" + "(" + num.getName() + "))", realTerm, realTerm * num.getDual() * std::log(k));
+}
+
+
+//Some standard functions
+
+namespace Dual {
+
+    DualNum pow(DualNum x, DualNum y){
+        long double realTerm = std::pow(x.getReal(), y.getReal());
+
+        return DualNum("((" + x.getName() + ")" + "^" + "(" + y.getName() + "))", realTerm, realTerm * ((x.getDual() * y.getReal() / x.getReal()) + y.getDual() * std::log(x.getReal())));
+    }
+
+    DualNum exp(DualNum x){
+        long double real = std::exp(x.getReal());
+
+        return DualNum("(e^" + x.getName() + ")", real, real * x.getDual());
+    }
+
+    DualNum log(DualNum x){
+        return DualNum("(log(" + x.getName() + "))", std::log(x.getReal()), x.getDual() / x.getReal());
+    }
+
+    DualNum relu(DualNum x){
+        if (x.getReal() <= 0){
+            x.setReal(0);
+            x.setDual(0);
+        }  
+
+        return x;
+    }
+
+    DualNum tanh(DualNum x){
+
+        DualNum posexp = Dual::exp(x);
+
+        DualNum negexp = Dual::exp(-1 * x);
+
+        return ((posexp - negexp) / ((posexp + negexp)));
+    }
+
+    DualNum sigmoid(DualNum x){
+        return (1 / (1 + Dual::exp(-1 * x)));
+    }
+
+    DualNum softmax (vector<DualNum> X, int index, DualNum sum = DualNum("summation(e)", 0, 0)) {
+
+        if (sum.getReal() == 0){
+            vector<DualNum>::iterator iter;
+            
+            for(iter = X.begin(); iter != X.end(); iter++){
+                sum += Dual::exp(*iter);
+            }   
+        }
+
+        return (Dual::exp(X[index]) / sum);
+    }
+
+    DualNum mse(vector<DualNum> y_train, vector<vector<DualNum>> yhat){
+        DualNum sum("0", 0, 0);
+
+        for (int i = 0; i < yhat.size(); i++){
+            sum += (y_train[i] - yhat[i][0]) * (y_train[i] - yhat[i][0]);
+        }
+
+        return (sum / (double)(yhat.size()));
+    }
+
+    vector<vector<DualNum>> originalMatMul(vector<vector<DualNum>> A, vector<vector<DualNum>> B){
+        int Arows = A.size();
+        int Acols = A[0].size();
+        int Brows = B.size();
+        int Bcols = B[0].size();
+
+        if (Acols != Brows){
+            throw std::runtime_error("The shapes of the Matrices don't match first=(" + std::to_string(Arows) + ", " + std::to_string(Acols) + ") and second = (" + std::to_string(Brows) + ", " + std::to_string(Bcols) + ")");
+        }
+
+        vector<vector<DualNum>> result(Arows, vector<DualNum>(Bcols, DualNum("0", 0)));
+
+        for (int i = 0; i < Arows; i++){
+            for (int j = 0; j < Bcols; j++){
+                for (int k = 0;  k < Brows; k++){
+                    result[i][j] += A[i][k] * B[k][j];
+                }
+            }
+        }
+
+        return result;
+    }
+
+    void partialMatMul(vector<vector<DualNum>>& A, vector<vector<DualNum>>& B, vector<vector<DualNum>>& result, int start, int end){
+        int Arows = A.size();
+        int Acols = A[0].size();
+        int Brows = B.size();
+        int Bcols = B[0].size();
+
+        if (Acols != Brows){
+            throw std::runtime_error("The shapes of the Matrices don't match first=(" + std::to_string(Arows) + ", " + std::to_string(Acols) + ") and second = (" + std::to_string(Brows) + ", " + std::to_string(Bcols) + ")");
+        }
+
+        if (Arows >= Bcols) {
+            for (int i = start; i < end; i++){
+                for (int j = 0; j < Bcols; j++){
+                    for (int k = 0;  k < Brows; k++){
+                        result[i][j] += A[i][k] * B[k][j];
+                    }
+                }
+            }
+        } else {
+            for (int i = 0; i < Arows; i++){
+                for (int j = start; j < end; j++){
+                    for (int k = 0;  k < Brows; k++){
+                        result[i][j] += A[i][k] * B[k][j];
+                    }
+                }
+            }
+        }
+    }
+
+    vector<vector<DualNum>> matmul(vector<vector<DualNum>>& A, vector<vector<DualNum>>& B){
+        int Arows = A.size();
+        int Acols = A[0].size();
+        int Brows = B.size();
+        int Bcols = B[0].size();
+
+        int no_of_threads = std::thread::hardware_concurrency();
+
+        int LargerDimension = (Arows >= Bcols) ? Arows : Bcols;
+
+        if (LargerDimension < no_of_threads * 5){
+            return Dual::originalMatMul(A, B);
+        }
+
+        vector<std::thread> threads;
+
+        vector<vector<DualNum>> result (Arows, vector<DualNum>(Bcols, DualNum("0", 0)));
+
+
+        int portion_for_each_thread = LargerDimension / no_of_threads;
+
+        int start = 0;
+        for (int i = 0; i < no_of_threads; i++){
+            int end = (i == (no_of_threads - 1) ? LargerDimension : start + portion_for_each_thread);
+            threads.emplace_back(partialMatMul, std::ref(A), std::ref(B), std::ref(result), start, end);
+            start = end;
+        }
+
+        for (auto& eachThread: threads){
+            eachThread.join();
+        }
+
+        return result;
+    }
+
+    void displayMatrix(vector<vector<DualNum>> mat){
+        int rows = mat.size();
+        int cols = mat[0].size();
+
+        for (int i = 0; i < rows; i++){
+            std::cout << "\n\n";
+            for (int j = 0; j < cols; j++){
+                std::cout << mat[i][j].getExpression() << "\t";
+            }
+        }
+    }
+
+}
+
+
+
+//some extra functions 
+
+long double partialDerivative(DualNum (*func)(vector<DualNum>), vector<DualNum> params, int paramIndex = 0, long double at = 1){
+    params[paramIndex].setDual(1);
+
+    if (at != 1){
+        params[paramIndex].setReal(at);
+    }
+
+    return ((*func)(params)).getDual();
+}
+
+template<typename Func>
+long double partialDerivative(Func func, vector<DualNum> params, int paramIndex = 0, long double at = 1){
+    params[paramIndex].setDual(1);
+
+    if (at != 1){
+        params[paramIndex].setReal(at);
+    }
+
+    return (func(params)).getDual();
+}
+
+long double partialDerivative(DualNum (*func)(DualNum), long double at = 1){
+    DualNum extX("x+E", at, 1);
+    DualNum x("x", at, 0);
+
+    return ((*func)(extX) - (*func)(x)).getDual();
+}
+
+#endif
